a8fb47d9b56bf37e3d71cb146b00cde87a528cee
Rachelle Luciana <125530468+rachellelcnn@users.noreply.github.com>
2023-07-15 11:51:11 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/transaksi', function () {
+   return view('transaksi');
 });
-
+Route::get('/barang', function () {
+   return view('barang');
+});
+Route::get('/edit', function () {
+   return view('edit');
+});
+Route::get('/create', function () {
+   return view('create');
+});
+Route::get('/', function () {
+    return view('index');
+ });
+ Route::get('users', 'UserChartController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
